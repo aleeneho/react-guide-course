42ef53e296135a6c8c614b1e828d72ef10a3c683
jianing <ning94mw@gmail.com>
2024-07-28 16:43:20 +0800

    deriving values, outputing question and registering answer

--- code/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react';
-
+import QUESTION from '../questions';
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswer, setUserAnswer] = useState([]);
+  const activeQuestionIndex = userAnswer.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswer((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
 
   return (
-    <p>Currently acrive Question</p>
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTION[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {QUESTION[activeQuestionIndex].answers.map((answer)=>(
+            <li key={answer} className='answer'>
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
