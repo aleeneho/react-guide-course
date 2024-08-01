2c655bf3a90962a90131a5bbaaa20dce34539eb8
jianing <ning94mw@gmail.com>
2024-08-01 17:05:36 +0800

    error component added

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -6,13 +6,25 @@ import Places from './Places.jsx';
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  cosnt [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching (true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+
+        if (!response.ok) {
+          // cosnt error = new Error('Failed o fetch places.')
+          throw new Error('Failed to fetch places.');
+        }
+      } catch (error) {
+        setError(error);
+      }
+
+      // setAvailablePlaces(resData.places);
       setIsFetching(false);
     }
 
