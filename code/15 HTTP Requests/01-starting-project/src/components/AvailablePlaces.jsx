f8ba7dc2ade3c37c6d3b56e5821c1f21996a64e1
jianing <ning94mw@gmail.com>
2024-08-01 16:40:43 +0800

    using async and awaiting

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -7,14 +7,23 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-    .then((response) => {
-      return response.json();
-    })
-    .then((resData) => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
       setAvailablePlaces(resData.places);
-    });
+    }
+
+    fetchPlaces();
   }, []);
+
+    // fetch('http://localhost:3000/places')
+    // .then((response) => {
+    //   return response.json();
+    // })
+    // .then((resData) => {
+    //   setAvailablePlaces(resData.places);
+    // });
+  // }, []);
 
   return (
     <Places
