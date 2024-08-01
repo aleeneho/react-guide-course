b00d2329a40384973bdd138ca2f22c1568487d41
jianing <ning94mw@gmail.com>
2024-08-01 17:31:55 +0800

    current position added

--- code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ code/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 //  localStorage.geyItem('places')
 
@@ -14,14 +15,21 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching (true);
 
       try {
-        const response = await fetch('http://localhost:3000/ne');
+        const response = await fetch('http://localhost:3000/places');
         const resData = await response.json();
 
         if (!response.ok) {
           // cosnt error = new Error('Failed o fetch places.')
           throw new Error('Failed to fetch places.');
         }
-        setAvailablePlaces(resData.places);
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          // console.log(position);
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude);
+          setAvailablePlaces(resData.places);
+          setIsFetching(false);
+        })
+
       } catch (error) {
         setError({message: error.message || 'Could not fetch places, please try again later.'});
       }
