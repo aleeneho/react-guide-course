d27cb72a23730aec513e63efadbcce67df0fc7d6
jianing <ning94mw@gmail.com>
2024-08-01 18:07:09 +0800

    sending data with 'POST' request

--- code/15 HTTP Requests/01-starting-project/src/http.js
+++ code/15 HTTP Requests/01-starting-project/src/http.js
@@ -11,3 +11,22 @@ export async function fetchAvailablePlaces () {
 
   return resData.places;
 }
+
+
+export async function updateUserPlaces () {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({places: places}),
+    headers: {
+      "Content-Type": "application/json",
+    }
+  });
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user data.")
+  }
+
+  return resData.message;
+}
