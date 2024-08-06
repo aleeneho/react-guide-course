5cdb2c64095f85f89c4bd595236ed550b5c29110
jianing <ning94mw@gmail.com>
2024-08-06 20:04:52 +0800

    mixing custom & build in validation logic

--- code/17 Forms User Input/01-starting-project/src/components/Signup.jsx
+++ code/17 Forms User Input/01-starting-project/src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -9,6 +13,10 @@ export default function Signup() {
     const data = Object.fromEntries(fd.entries())
     data.acquisition = acquisitionChannel;
 
+    if (data.password !== data['confirm-password']) {
+      setPasswordsAreNotEqual(true);
+      return
+    }
     // event.target.reset();
   }
 
@@ -36,6 +44,9 @@ export default function Signup() {
             name="confirm-password"
             required
           />
+          <div className='control-error'>
+            {passwordsAreNotEqual && <p>Passwords do not match.</p>}
+          </div>
         </div>
       </div>
 
