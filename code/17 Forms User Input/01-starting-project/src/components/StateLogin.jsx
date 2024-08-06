7c86571343e561acae5479ac1107615162e61dd2
jianing <ning94mw@gmail.com>
2024-08-06 19:15:18 +0800

    validating input upon for submission

--- code/17 Forms User Input/01-starting-project/src/components/StateLogin.jsx
+++ code/17 Forms User Input/01-starting-project/src/components/StateLogin.jsx
@@ -1,7 +1,9 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
-  const emial = useRef();
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
+  const email = useRef();
   const password = useRef();
 
   function handleSubmit(event) {
@@ -9,8 +11,15 @@ export default function Login() {
 
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
-    
 
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
   }
 
   return (
@@ -26,6 +35,9 @@ export default function Login() {
             name="email"
             ref={email}
              />
+            <div className='control-error'>
+              {emailIsInvalid && <p>PLease enter a valid email address.</p>}
+            </div>
         </div>
 
         <div className="control no-margin">
