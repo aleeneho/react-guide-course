bcd362357c6d505e3c20e9622148215a8101efb1
jianing <ning94mw@gmail.com>
2024-08-06 18:44:35 +0800

    validating input upon lost focus (blur)

--- code/17 Forms User Input/01-starting-project/src/components/Login.jsx
+++ code/17 Forms User Input/01-starting-project/src/components/Login.jsx
@@ -8,8 +8,13 @@ export default function Login() {
     password: "",
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
   const emailIsInvalid =
-    enteredValues.email !== '' && !enteredValues.email.includes('@')
+    didEdit.email && !enteredValues.email.includes('@')
 
 
   function handleSubmit(event) {
@@ -17,10 +22,21 @@ export default function Login() {
   }
 
   function handleInputChange(identifier, value) {
-    setEnteredValues(preValues => ({
+    setEnteredValues((preValues) => ({
       ...preValues,
       [identifier]: value
     }))
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
   }
 
   // function handleEmailChange(event) {
@@ -42,6 +58,7 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur('email')}
             onChange={(event) => handleInputChange('email', event.target.value)}
             value={enteredValues.email}
              />
