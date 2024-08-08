041dc4b1435a87e2e47f01e1df1c27368a24c143
jianing <ning94mw@gmail.com>
2024-08-08 17:02:21 +0800

    working with multiple state properties

--- code/19 Redux Basics/01-starting-project/src/store/index.js
+++ code/19 Redux Basics/01-starting-project/src/store/index.js
@@ -1,21 +1,36 @@
 import { createStore } from 'redux';
 
-const conuterReducer = (state = { counter: 0 }, action) => {
+const initialState = {
+  counter: 0,
+  showCounter: true,
+};
+
+const conuterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'incrementBy5') {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if(action.type === 'toggle') {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
