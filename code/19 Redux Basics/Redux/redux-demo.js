4874dc6c105982e120ee9ad1a6dced0545a70ad8
jianing <ning94mw@gmail.com>
2024-08-08 13:44:42 +0800

    dispatch the increment and decrement

--- code/19 Redux Basics/Redux/redux-demo.js
+++ code/19 Redux Basics/Redux/redux-demo.js
@@ -1,9 +1,19 @@
 const redux = require('redux');
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return  state;
 };
 
 const store = redux.createStore(counterReducer);
@@ -11,9 +21,10 @@ const store = redux.createStore(counterReducer);
 
 const counterSubcriber = () => {
   const latestState = store.getState();
-
+  console.log(latestState);
 };
 
 store.subscribe(counterSubcriber);
 
 store.dispatch({type: 'increment'});
+store.dispatch({type: 'decrement'});
