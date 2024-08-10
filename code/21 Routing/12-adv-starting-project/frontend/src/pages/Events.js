6bcf0793ba6350d75cef5084579ef045f9bd6c18
jianing <ning94mw@gmail.com>
2024-08-10 19:12:13 +0800

    Output a list of dummy events to the EventsPage

--- code/21 Routing/12-adv-starting-project/frontend/src/pages/Events.js
+++ code/21 Routing/12-adv-starting-project/frontend/src/pages/Events.js
@@ -1,9 +1,33 @@
+import { Link } from 'react-router-dom';
+
+const DUMMY_EVENTS = [
+  {
+    id: 'e1',
+    title: 'Some Event',
+  },
+  {
+    id: 'e2',
+    title: 'Another Event',
+  },
+  {
+    id: 'e3',
+    title: 'Yet Another Event',
+  },
+]
+
 function EventsPage () {
   return (
-    <h1>
-      Event Page
-    </h1>
-  )
+    <>
+      <h1>
+        Event Page
+      </h1>
+      <ul>
+        {DUMMY_EVENTS.map(event =>
+          <li key={event.id}><Link to={`/events/${event.id}`}>{event.title}</Link></li>
+        )}
+      </ul>
+    </>
+  );
 }
 
 export default EventsPage;
