f675422d213b7be204d301b113d5c9e6d9df3fc2
jianing <ning94mw@gmail.com>
2024-08-10 19:14:00 +0800

    Output the ID of the selected event on the EventDetailPage

--- code/21 Routing/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ code/21 Routing/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,8 +1,15 @@
+import { useParams } from 'react-router-dom';
+
 function EventDetailPage() {
+  const params = useParams();
+
   return (
-    <h1>
-      Event Detail Page
-    </h1>
+    <>
+      <h1>
+        Event Detail Page
+      </h1>
+      <p>Event ID:{params.eventId}</p>
+    </>
   )
 }
 
