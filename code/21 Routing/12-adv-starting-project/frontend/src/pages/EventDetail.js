ad14cfcb1c8847b91f7b9066f29b9a239c2daae7
jianing <ning94mw@gmail.com>
2024-08-15 22:00:58 +0800

    Submitting data programmatically

--- code/21 Routing/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ code/21 Routing/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData, json } from "react-router-dom";
+import { useRouteLoaderData, json, redirect } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 
@@ -22,5 +22,19 @@ export async function loader({request, params}){
     throw json({message: 'Something went wrong!'}, {status: 500});
   } else {
     return response;
+  }
+}
+
+export async function action ({request, params}) {
+  const eventId = params.eventId;
+  const response = await fetch('http://localhost:8080/events/' + eventId, {
+    method: request.method,
+  });
+
+
+  if (!response.ok) {
+    throw json({message: 'Could not delete event'}, {status: 500});
+  } else {
+    return redirect('/events');
   }
 }
