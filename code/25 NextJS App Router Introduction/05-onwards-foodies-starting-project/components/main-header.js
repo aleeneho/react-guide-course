2f2feb22bb7bf92ee5b4c6a5a62199fa62d74f3f
jianing <ning94mw@gmail.com>
2024-08-22 22:24:23 +0800

    using more custom components and css

--- code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header.js
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header.js
@@ -3,25 +3,29 @@ import Image from 'next/image'
 
 import logoImg from '@/assets/logo.png'
 import classes from './main-header.module.css'
+import MainHeaderBackground from './main-header-background'
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href='/'>
-        <Image src={logoImg} alt='A plate with food on it' priority />
-        Next Level Food
-      </Link>
+    <>
+     <MainHeaderBackground/>
+      <header className={classes.header}>
+        <Link className={classes.logo} href='/'>
+          <Image src={logoImg} alt='A plate with food on it' priority />
+          Next Level Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href='/meals'>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href='/community'>Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href='/meals'>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href='/community'>Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   )
 }
