b741dd516b57df64143bcf20ed86719ce96ae270
jianing <ning94mw@gmail.com>
2024-08-22 21:47:47 +0800

    difference page routers created

---
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/app/community/page.js
@@ -0,0 +1,8 @@
+export default function CommunityPage({ page }) {
+  return (
+    <div>
+      <h1>Community</h1>
+
+    </div>
+  );
+}
---
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/app/meals/page.js
@@ -0,0 +1,8 @@
+export default function MealsPage({ page }) {
+  return (
+    <div>
+      <h1>Meal Page</h1>
+      
+    </div>
+  );
+}
