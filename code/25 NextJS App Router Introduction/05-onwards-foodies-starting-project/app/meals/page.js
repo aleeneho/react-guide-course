9353d2121ca1960174b7049d06bb1ff940fcc5c0
jianing <ning94mw@gmail.com>
2024-08-24 17:12:32 +0800

    outputting meals data and image with unknown dimension

--- code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/app/meals/page.js
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,8 +1,21 @@
+import MealsGrid from '@/components/meals/meals-grid';
+import classes from './page.module.css';
+import Link from 'next/link';
+
 export default function MealsPage({ page }) {
   return (
-    <div>
-      <h1>Meal Page</h1>
-      
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals for Every Occasion, created {''}
+          <span className={classes.highlight}>just for you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it youeself. It is easy</p>
+        <p className={classes.cta}><Link href='/meals/share'>Share your recipe</Link></p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
