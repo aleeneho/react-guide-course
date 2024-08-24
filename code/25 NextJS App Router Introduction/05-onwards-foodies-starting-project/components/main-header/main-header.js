e38ef674745d967d62b9833ae4c715c31ed234dc
jianing <ning94mw@gmail.com>
2024-08-24 16:52:28 +0800

    using client components efficiently

--- code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ code/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -1,11 +1,17 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 
 import logoImg from '@/assets/logo.png'
 import classes from './main-header.module.css'
 import MainHeaderBackground from './main-header-background'
+// import { usePathname } from 'next/navigation'
+import NavLink from './nav-link'
 
 export default function MainHeader() {
+  // const path = usePathname();
+
   return (
     <>
      <MainHeaderBackground/>
@@ -18,10 +24,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href='/meals'>Browse Meals</Link>
+              <NavLink href='/meals'>Browse Meals</NavLink>
             </li>
             <li>
-              <Link href='/community'>Community</Link>
+              <NavLink href='/community'>Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
