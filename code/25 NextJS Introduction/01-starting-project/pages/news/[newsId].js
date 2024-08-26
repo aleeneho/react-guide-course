9de42561d8aa16cf44627056b40d03104831206e
jianing <ning94mw@gmail.com>
2024-08-26 17:00:20 +0800

    extracting dynamic parameter values

--- code/25 NextJS Introduction/01-starting-project/pages/news/[newsId].js
+++ code/25 NextJS Introduction/01-starting-project/pages/news/[newsId].js
@@ -1,5 +1,10 @@
+import { useRouter} from 'next/router';
 
 function DetailPage () {
+  const router = useRouter();
+
+  const newsId = router.query.newsId; // newsId is the name of the file in the pages folder
+
   return (
     <h1>The Detail Page</h1>
   )
