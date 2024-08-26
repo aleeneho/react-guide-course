1fd0fcd9f8c69ca6130cf6c57ba5f4d1e7c1c279
jianing <ning94mw@gmail.com>
2024-08-26 17:09:08 +0800

    linking between pages

--- code/25 NextJS Introduction/01-starting-project/pages/news/index.js
+++ code/25 NextJS Introduction/01-starting-project/pages/news/index.js
@@ -1,7 +1,19 @@
+import { Link } from 'next/link';
+import { Fragment } from 'react';
 
 function NewsPage () {
   return (
-    <h1>The News Page</h1>
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/nextjs-is-a-great-framework'>
+            NextJS is a Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
   )
 }
 
