15a397fd5fe617be6656bd13fabe09ad7d494e5f
jianing <ning94mw@gmail.com>
2024-08-31 19:20:44 +0800

    managing multi-component state with the context API

--- code/27 Patterns/01-starting-project/src/App.jsx
+++ code/27 Patterns/01-starting-project/src/App.jsx
@@ -8,14 +8,14 @@ function App() {
         <h2>Why work with us?</h2>
 
         <Accordion className='accordion'>
-          <AccordionItem className='accordion-item' title='We got 20 years of experience'>
+          <AccordionItem id='experience' className='accordion-item' title='We got 20 years of experience'>
             <article>
               <p>you can&apos;t go wrong with us.</p>
               <p>We are in the business of planninh highly individualized vacation trips for more than 20 years.</p>
             </article>
           </AccordionItem>
 
-          <AccordionItem className='accordion-item' title='We are working with us.'>
+          <AccordionItem id='local' className='accordion-item' title='We are working with us.'>
             <article>
               <p>We are not doing this along from our office.</p>
               <p>Instead, we are working with local guides to ensure a safe and pleasant vacation.</p>
--- code/27 Patterns/01-starting-project/src/components/Accordion/accordion.jsx
+++ code/27 Patterns/01-starting-project/src/components/Accordion/accordion.jsx
@@ -1,8 +1,39 @@
+import { createContext, useState, useContext } from "react"
+
+const AccordionContext = createContext()
+
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext)
+
+  if (!ctx) {
+    throw new Error('AccordionItem must be wrapped within an Accordion')
+  }
+
+  return ctx;
+}
 
 export default function Accordion({ children, className }) {
+  const [openItemId, setOpenItemId] = useState(null)
+
+  function openItem(id) {
+    setOpenItemId(id);
+  }
+
+  function closeItem() {
+    setOpenItemId(null);
+  }
+
+  const contextValue = {
+    openItemId: openItemId,
+    openItem: openItem,
+    closeItem: closeItem,
+   }
+
   return (
-  <ul className={className}>
-    {children}
-  </ul>
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>
+        {children}
+      </ul>
+    </AccordionContext.Provider>
   )
 }
