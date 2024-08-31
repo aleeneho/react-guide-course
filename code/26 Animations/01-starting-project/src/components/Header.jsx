7bfc1838fbd6280bc57818ff7ebecbedf4c00e09
jianing <ning94mw@gmail.com>
2024-08-31 16:24:46 +0800

    aniamting colors and working with keyframes

--- code/26 Animations/01-starting-project/src/components/Header.jsx
+++ code/26 Animations/01-starting-project/src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <motion.button whileHover={{ scale: 1.1 }} trnasition={{ typr: 'spring', stiffness: 500 }} onClick={handleStartAddNewChallenge} className="button">
+        <motion.button whileHover={{ scale: 1.1, backgroundColor: '#8b11f0' }} transition={{ typr: 'spring', stiffness: 500 }} onClick={handleStartAddNewChallenge} className="button">
           Add Challenge
         </motion.button>
       </header>
