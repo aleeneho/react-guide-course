144097ae45f19a0f8b010812832f4a07e4cb78ee
jianing <ning94mw@gmail.com>
2024-09-04 17:59:21 +0800

    minor update test code and info

---
+++ code/28 Testing/01-starting-project/src/Async.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import Async from './Async';
+
+describe ('Async Component', () => {
+  TextDecoderStream('renders posts if request succeeds', () => {
+    render(<Async />);
+
+    
+
+    screen.
+  })
+});
