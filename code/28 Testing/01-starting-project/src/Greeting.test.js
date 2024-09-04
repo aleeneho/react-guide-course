dcfbf2f5d087ae995e3ce2de61dc69df28c1be2f
jianing <ning94mw@gmail.com>
2024-09-04 17:16:58 +0800

    first test wrote

---
+++ code/28 Testing/01-starting-project/src/Greeting.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Greeting from './Greeting';
+
+describe('Greeting component', () => {
+  test('renders Hellow World as as text', () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    //...nothing
+
+    //Assert
+    const helloWorldElement = screen.getByText('Hello World!');
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+})
